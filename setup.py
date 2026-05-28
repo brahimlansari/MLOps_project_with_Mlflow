c0c3f8219834fb9dc8377bef79d58dfab010f161
Lansari ibrahim <brahimlansari2002@gmail.com>
2026-05-28 11:05:19 -0400

    Setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read() 
+    
+    
+__version__ = '0.0.0'
+
+REPO_NAME = "MLOps_project_with_Mlflow"
+AUTHOR_USER_NAME = "brahimlansari"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "brahimlansari2002@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,  
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Machine Learning project template with Mlflow for MLOps.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https//github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
